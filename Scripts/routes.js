c63d1f24071ef2915892fbf8ada5c4201b369c4b
Pamela Hammond <hammondpamelac@gmail.com>
2021-08-23 16:01:53 -0400

    added new folders and updated helpers

---
+++ Scripts/routes.js
@@ -0,0 +1,49 @@
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const express = require('express');
+const fb = express.Router();
+const { readFromFile, writeToFile, readAndAppend } = require('../Helpers/fsUtils')
+
+
+fb.get('/', (req, res) =>
+  readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+);
+
+fb.post('/', (req, res) => {
+    
+    const { title, text} = req.body;
+
+    if (req.body) {
+    
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './Develop/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
+
+  router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    const notes = JSON.parse(fs.readFileSync('./Develop/db.json', 'utf-8'));
+  
+    const noteIndex = notes.findIndex(note => note.note_id === id);
+  
+    notes.splice(noteIndex, 1);
+  
+    writeToFile("./Develop/db.json", notes);
+    return res.send();
+  });
+
+  module.exports = fb;
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
-const pulls = require('./Develop/db.json')
-const PORT = process.env.port || 3001;
+const pulls = require('./Develop/db.json');
+const path = require('path');
 
 const app = express();
-
+const PORT = process.env.port || 3001;
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/api', (req, res) => res.json(pulls));
 
-app.listen(PORT, () => console.log('App listening at http://localhost:${PORT}!'));
+// route to index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// route to notes.html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    res.json(pulls);
+    console.info(`${req.method} request received to retrieve past notes`);
+});
+
+app.post('/api/notes', (req, res) => {
+    res.json(pulls);
+    console.info(`${req.method} request received to add a new note`);
+});
+
+app.listen(PORT, () => console.log('Application listening at http://localhost:${PORT}!'));
+
